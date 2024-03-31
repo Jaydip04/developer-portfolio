2db5766af1b21b384c468c881669cf3be9d71b98
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-03-31 17:21:25 +0530

    update worflow

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/developer-portfolio',
+  assetPrefix: '/developer-portfolio/',
+}
+
+module.exports = nextConfig
