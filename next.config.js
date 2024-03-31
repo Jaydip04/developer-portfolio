de629383e83663da2fdeb8af7608f1b17a90c069
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-03-31 17:43:18 +0530

    update workflow

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: { unoptimized: true },
   basePath: '/developer-portfolio',
   assetPrefix: '/developer-portfolio/',
+  distDir:"build",
 }
 
 module.exports = nextConfig
