1c626e84ada08cc0ad169a091ba5bb068f089e17
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-04-07 11:10:42 +0530

    update workflow

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
-  output: 'standalone',
-  images: { unoptimized: true },
-  basePath: '/developer-portfolio',
-  assetPrefix: '/developer-portfolio/',
-  distDir: 'build',
+  output: 'standalone', // This line enables standalone output
+  images: { unoptimized: true }, // Optional: Enables control over image optimization
+  basePath: '/developer-portfolio', // Optional: Sets the base path for routing
+  assetPrefix: '/developer-portfolio/', // Optional: Configures asset paths
+  // distDir: 'build', // Optional: Sets the output directory name (default: '.next')
+  distDir: '.next', // Optional: Sets the output directory name (default: '.next')
 }
 
 module.exports = nextConfig
